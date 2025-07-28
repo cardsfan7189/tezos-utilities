2295ae94e41abe82d1ffd1a8d050fb7142b916f9
Tez Nebraska <teznebraska@gmail.com>
2025-07-27 22:07:32 -0500

    initial version

--- monitor_rewards.py
+++ monitor_rewards.py
@@ -7,17 +7,22 @@ def send_msg(message,topic_arn):
     sns = boto3.client('sns')
     topic_arn = topic_arn
     sns.publish(TopicArn=topic_arn,
+                Subject="[URGENT] Action needed today - Missed payment",
                 Message=message)
 
-def load_overdelegators(s3,file_name):
+def load_cycle_info(s3,file_name,file_path):
     #file_name = "C:\\users\\drewa\\downloads\\temp.txt"
-    s3.download_file('teznebraska', 'overdelegators.json', file_name)
-    fo = open(file_name,"r+")
-    overdelegations = json.load(fo)
+    s3.download_file('teznebraska', file_name, file_path + file_name)
+    fo = open(file_path + file_name,"r+")
+    cycle = fo.read()
     fo.close()
+    return cycle
 
-    return overdelegations["overdelegators"]
+def upload_cycle_info(s3,file_name,file_path,cycle):
+    with open(file_path + file_name, 'w') as f:
+        f.write(cycle)
 
+    s3.upload_file(file_path + file_name, "teznebraska", file_name)
 def lambda_handler(event,context):
     print("Lambda handler")
     main(   )
@@ -25,32 +30,44 @@ def lambda_handler(event,context):
 def main():
     s3 = boto3.client('s3')
     topic_arn = "arn:aws:sns:us-east-1:917965627285:dell_check"
-    block_len = 10  # seconds
-    curr_tezpay_delay = 1433 # cycle pos
+
+    prev_cycle = 0
+    file_name = "monitor_rewards_cycle.txt"
+    file_path = "/tmp/"
+    file_path = "C:\\Users\\DREWA\\Downloads"
+    block_len = 8  # seconds
+    curr_tezpay_delay = 608 # cycle pos  966 + 14
     event_sched_rate = 1800 # seconds
-    buffer = 6 # levels
-    upper_limit = curr_tezpay_delay + (event_sched_rate / block_len ) + buffer
-    lower_limit = curr_tezpay_delay - (event_sched_rate / block_len ) - buffer
-    print(upper_limit,lower_limit)
+    buffer = 960 / 8  # number of seconds in 15 minute interval divided by seconds per level
+    curr_tezpay_delay_with_padding = curr_tezpay_delay + (event_sched_rate / block_len ) + buffer
+    lower_limit = curr_tezpay_delay
+    #print(upper_limit,lower_limit)
     payor_address = "tz1fnU3mjTn8aH2tJ5TcnS5HnfP4wUEhjE7j"
     resp = requests.get("https://rpc.tzkt.io/mainnet/chains/main/blocks/head")
     data = resp.json()
-    cycle_pos = data["metadata"]["level_info"]["cycle_position"]
+    #cycle_pos = data["metadata"]["level_info"]["cycle_position"]
     cycle = data["metadata"]["level_info"]["cycle"]
-    level = data["metadata"]["level_info"]["level"]
-    prev_cycle = cycle - 1
-    if cycle_pos < lower_limit or cycle_pos > upper_limit:
-        return
-    starting_level = level - cycle_pos
-    overdelegators = load_overdelegators(s3,"/tmp/temp.txt")
+    last_cycle_processed = load_cycle_info(s3,file_name,file_path)
+    print("Last cycle processed {0}, this cycle {1}".format(last_cycle_processed,cycle))
+
+    if last_cycle_processed == str(cycle):
+        return 0
+
+    resp = requests.get("https://api.tzkt.io/v1/cycles/" + str(cycle))
+    cycle_info = resp.json()
+    firstLevel = cycle_info["firstLevel"]
+    #level = data["metadata"]["level_info"]["level"]
+    #prev_cycle = cycle - 1
+    #if cycle_pos < lower_limit or cycle_pos > upper_limit:
+      #  return
+    starting_level = firstLevel + curr_tezpay_delay_with_padding
+    #overdelegators = load_overdelegators(s3,"/tmp/temp.txt")
     base_url = "https://api.tzkt.io/v1/rewards/split/tz1ffYUjwjduZkoquw8ryKRQaUjoWJviFVK6/"
-    resp = requests.get(base_url + str(prev_cycle) + "?limit=500")
+    resp = requests.get(base_url + str(last_cycle_processed) + "?limit=500")
     first_delegator = ""
     last_delegator = None
     data = resp.json()
     for delegator in data["delegators"]:
-        if delegator["address"] in overdelegators:
-            continue
         if first_delegator == "":
             first_delegator = delegator["address"]
         elif delegator["delegatedBalance"] > 9999999:
@@ -65,7 +82,8 @@ def main():
     data = resp.json()
     count_2 = len(data)
     if count_1 == 0 or count_2 == 0:
+        #if True:
         send_msg("Rewards payments may have been missed", topic_arn)
-
+    upload_cycle_info(s3,file_name,file_path,str(cycle))
 
 #main()
